79a65f351728d8b20fe649eec2486de4d10343cd
nwroot <nwroot@protonmail.com>
2022-11-24 11:00:09 -0300

    added drawing, gitignore

--- src/pizarraproject/Pizarra.java
+++ src/pizarraproject/Pizarra.java
@@ -1,32 +1,97 @@
-
 package pizarraproject;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.event.MouseInputAdapter;
 
+public class Pizarra extends JPanel {
 
-public class Pizarra extends JPanel{
     private JLabel label;
-    public Pizarra() throws IOException{
-        this.setPreferredSize(new Dimension(1300, 600));
+
+    public Pizarra() throws IOException {
+        this.drawn = new ArrayList<Drawn>();
+
+        this.setPreferredSize(new Dimension(1000, 600));
         this.setBackground(new Color(250, 250, 250));
-        /* Tried to add image, need fixes.
-        label = new JLabel();
-        BufferedImage bufferedImage = ImageIO.read(new File("Whiteboard.png"));
-        Image image = bufferedImage.getScaledInstance(1350,600, Image.SCALE_SMOOTH);
-        ImageIcon icon = new ImageIcon(image);
-        label.setIcon(icon);
-        this.add(label);
-        */
-        
-        
+        Listener listener = new Listener();
+        this.addMouseListener(listener);
+        this.addMouseMotionListener(listener);
+
+    }
+
+    private ArrayList<Drawn> drawn;
+    private Drawn curr;
+
+    class Listener extends MouseInputAdapter {
+
+        @Override
+        public void mousePressed(MouseEvent m) {
+            curr = new Drawn();
+            curr.pixels.add(new Point(m.getX(), m.getY()));
+            repaint();
+        }
+
+        public void mouseDragged(MouseEvent m) {
+            curr.pixels.add(new Point(m.getX(), m.getY()));
+            System.out.println("drag");
+            repaint();
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent me) {
+            drawn.add(curr);
+            //this.curr = null;
+            repaint();
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent me) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent me) {
+
+        }
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+
+        for (int i = 0; i < drawn.size(); i++) {
+            for (int j = 0; j < drawn.get(i).pixels.size(); j++) {
+                Point p = drawn.get(i).pixels.get(j);
+                g.drawRect(p.x, p.y, 1, 1);
+            }
+        }
+        if (this.curr != null) {
+            for (int i = 0; i < this.curr.pixels.size(); i++) {
+                Point p = this.curr.pixels.get(i);
+                g.drawRect(p.x, p.y, 1, 1);
+            }
+        }
+    }
+
+    private static class Drawn {
+
+        public Drawn() {
+            pixels = new ArrayList<Point>();
+        }
+
+        public ArrayList<Point> pixels;
     }
 }
