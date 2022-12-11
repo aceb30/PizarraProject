6c80c827ebb90afe68317fec91fd761a78817945
nwroot <nwroot@protonmail.com>
2022-12-11 19:39:52 -0300

    add uml generalization

---
+++ src/pizarraproject/drawable/DrawableUMLGeneralization.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pizarraproject.drawable;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author nwroot
+ */
+public class DrawableUMLGeneralization extends DrawableUMLLine {
+        @Override
+    public void paint(Graphics g) {
+        drawArrowLine(g, x1, y1, x2, y2, 10, 10, true);
+    }
+}
